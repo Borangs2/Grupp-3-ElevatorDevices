eae5442933c9ed5268dd8a865c2ef5614245730c
Andreas Boräng <andbor02@gmail.com>
2022-11-01 11:26:18 +0100

    Added the ability to add multiple elevators

--- Elevator1/CreateElevator.cs
+++ Elevator1/CreateElevator.cs
@@ -1,38 +1,137 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
+using System.Net.Http.Json;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Shared;
+using static Microsoft.Azure.Amqp.Serialization.SerializableType;
 
 namespace Elevator1
 {
     public  class CreateElevator
     {
-        private readonly int _interval;
         public string StatusMessage { get; set; }
-        public Guid Id { get; set; } = Guid.NewGuid();
-
-        public CreateElevator(Guid id, string name, int interval)
+        public ElevatorModel Elevator { get; set; }
+        private readonly string _connectUrl;
+        public CreateElevator(ElevatorModel elevator, string connectUrl)
         {
-            Id = id;
-            _interval = interval;
+            Elevator = elevator;
+            _connectUrl = connectUrl;
+
         }
-        //TODO: Lägg til allt annat från program så att den skapar alla devices
+
+        string DbConnectionString =
+            "Server=tcp:kyh-devops.database.windows.net,1433;Initial Catalog=Kyh-Agile Grupp 3;Persist Security Info=False;User ID=CloudSA37b586b4;Password=Andreas1!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        string IoTHubConnectionString =
+            "HostName=Grup-3-Devops.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=cXHMcmESQtlUTvhMJ8q5aQvzf9aPcWQ9JAN6fCc2r2Q=";
 
         public  async Task CreteElevator()
         {
-            StatusMessage = "Initializing device: -------";
-            await Task.Delay(_interval);
-            StatusMessage = "Initializing device: █------";
-            await Task.Delay(_interval);
-            StatusMessage = "Initializing device: ██-----";
-            await Task.Delay(_interval);
-            StatusMessage = "Initializing device: ███----";
-            await Task.Delay(_interval);
-            StatusMessage = "Initializing device: ████---";
-            await Task.Delay(_interval);
+            StatusMessage = "Initializing device:               ----------";
 
+
+            using IDbConnection connection = new SqlConnection(DbConnectionString);
+
+            if (await connection.QueryFirstOrDefaultAsync<Guid>("SELECT Id FROM Elevators WHERE Id = @DeviceId", new { DeviceId = Elevator.Id }) == Guid.Empty)
+            {
+                StatusMessage = "New device detected:               ██--------";
+                
+                await connection.ExecuteAsync(
+                    "INSERT INTO Elevators (id, ConnectionString) VALUES (@deviceId, @ConnectionString)",
+                    new {deviceId = Elevator.Id, ConnectionString = "" });
+            }
+            else
+            {
+                StatusMessage = "Device detected:                   ██--------";
+            }
+
+
+            var deviceConnectionString = await connection.QueryFirstOrDefaultAsync<string>("SELECT ConnectionString FROM Elevators WHERE Id = @DeviceId", new { DeviceId = Elevator.Id });
+            if (string.IsNullOrEmpty(deviceConnectionString))
+            {
+                StatusMessage = "Initializing Connection string:    ████------";
+
+                using var http = new HttpClient();
+                var result = await http.PostAsJsonAsync($"{_connectUrl}?DeviceId={Elevator.Id}", new { DeviceId = Elevator.Id });
+                StatusMessage = "Connecting device:                 ██████----";
+
+                deviceConnectionString = await result.Content.ReadAsStringAsync();
+                await connection.ExecuteAsync("UPDATE Elevators SET ConnectionString = @ConnectionString WHERE Id = @DeviceId",
+                    new { DeviceId = Elevator.Id, ConnectionString = deviceConnectionString });
+            }
+
+            DeviceClient _deviceClient = DeviceClient.CreateFromConnectionString(IoTHubConnectionString, Elevator.Id.ToString());
+
+            StatusMessage = "Updating twin properties           ████████--";
+            var twinCollection = new TwinCollection();
+            twinCollection["deviceName"] = Elevator.Name.ToLower();
+            twinCollection["status"] = Elevator.Status.ToString().ToLower();
+            twinCollection["doorStatus"] = Elevator.DoorStatus;
+            twinCollection["currentLevel"] = Elevator.CurrentLevel;
+            twinCollection["targetLevel"] = Elevator.TargetLevel;
+            await _deviceClient.UpdateReportedPropertiesAsync(twinCollection);
+
+            var twin = await _deviceClient.GetTwinAsync();
+
+
+            await _deviceClient.SetMethodHandlerAsync("ChangeLevel", ChangeLevel, null);
+
+            Elevator.IsConnected = true;
+            StatusMessage = "Device Connected:                  ██████████";
+
+            await Task.Delay(2000);
+            KeepActive(_deviceClient);
+
+
+            while (true)
+            {
+                StatusMessage = $"{Elevator.Name}: Current level: {Elevator.CurrentLevel}, Target level: {Elevator.TargetLevel}                      ";
+                await Task.Delay(TimeSpan.FromSeconds(2));
+            }
+
+        }
+
+        async Task KeepActive(DeviceClient deviceClient)
+        {
+            var twinCollection = new TwinCollection();
+            while (true)
+            {
+                twinCollection["update"] = "jag existerar";
+                await deviceClient.UpdateReportedPropertiesAsync(twinCollection);
+                await Task.Delay(TimeSpan.FromHours(1));
+            }
+
+        }
+
+        Task<MethodResponse> ChangeLevel(MethodRequest methodRequest, object userContext)
+        {
+            //var newLevel = userContext.
+            //doorStatus = false;
+            //targetLevel = newLevel;
+            //if (newLevel != currentLevel)
+            //{
+            //    twinCollection["doorStatus"] = doorStatus;
+            //    twinCollection["targetLevel"] = targetLevel;
+            //    _deviceClient.UpdateReportedPropertiesAsync(twinCollection);
+
+            //    while (currentLevel != targetLevel)
+            //    {
+            //        if (currentLevel > targetLevel)
+            //            currentLevel -= 1;
+            //        if (currentLevel < targetLevel)
+            //            currentLevel += 1;
+
+            //        Task.Delay(TimeSpan.FromSeconds(4));
+            //    }
+            //}
+
+            return Task.FromResult(new MethodResponse(new byte[0], 200));
         }
     }
 }
--- Elevator1/Program.cs
+++ Elevator1/Program.cs
@@ -2,6 +2,7 @@
 using System.Data.SqlClient;
 using System.Net.Http.Json;
 using System.Net.Mime;
+using System.Runtime.CompilerServices;
 using Dapper;
 using Elevator1;
 using Microsoft.Azure.Devices.Client;
@@ -9,33 +10,23 @@ using Microsoft.Azure.Devices.Shared;
 
 
 
-
-
 string connectUrl = "http://localhost:*port*/api/devices/connect";
-string IoTHubConnectionString =
-    "HostName=Grup-3-Devops.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=cXHMcmESQtlUTvhMJ8q5aQvzf9aPcWQ9JAN6fCc2r2Q=";
-string DbConnectionString =
-    "Server=tcp:kyh-devops.database.windows.net,1433;Initial Catalog=Kyh-Agile Grupp 3;Persist Security Info=False;User ID=CloudSA37b586b4;Password=Andreas1!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-bool poweredState = false;
-TimeSpan interval = TimeSpan.FromHours(1);
-bool connected = false;
 
 
-Guid deviceId = Guid.Parse("04ddc77d-d1c3-41dd-a3e8-14896f1d9b63");
-string deviceName = "Optima 563";
-ElevatorStatus status = ElevatorStatus.Disabled;
-bool doorStatus = false;
-int currentLevel = 0;
-int targetLevel = 0;
+
+
+//Replace port
+Console.Write("What port is being used: ");
+connectUrl = connectUrl.Replace("*port*", Console.ReadLine());
+Console.Clear();
 
 List<CreateElevator> elevators = new List<CreateElevator>
 {
-    new CreateElevator(Guid.NewGuid(), "hej", 1000),
-    new CreateElevator(Guid.NewGuid(), "hej", 1250),
+    new CreateElevator(new ElevatorModel{Id = Guid.Parse("0e3c55f3-ea28-4551-8b06-c9dd9b6b3711"), Interval = 1000, Name = "test1"}, connectUrl),
+    new CreateElevator(new ElevatorModel{Id = Guid.Parse("25967951-1de4-4608-a42c-0938e1c6de83"), Interval = 1000, Name = "test2"}, connectUrl),
 };
+
 Console.CursorVisible = false;
-
-
 string statusBoard = "";
 int amountOfElevator = 0;
 foreach (var elevator in elevators)
@@ -44,7 +35,6 @@ foreach (var elevator in elevators)
     elevator.CreteElevator();
     statusBoard += $"{amountOfElevator}: {elevator.StatusMessage} \n";
 }
-
 
 Console.Write(statusBoard);
 while (true)
@@ -64,94 +54,4 @@ while (true)
         Console.SetCursorPosition(0, 0);
     }
     Console.Write($"{statusBoard}");
-}
-
-
-Console.WriteLine("Initializing Device. Please wait ...");
-
-//Replace port
-Console.Write("What port is being used: ");
-connectUrl = connectUrl.Replace("*port*", Console.ReadLine());
-
-using IDbConnection connection = new SqlConnection(DbConnectionString);
-
-if (await connection.QueryFirstOrDefaultAsync<Guid>("SELECT Id FROM Elevators WHERE Id = @DeviceId", new { DeviceId = deviceId }) == Guid.Empty)
-{
-    Console.WriteLine("New device detected. Inserting device to database ...");
-
-    await connection.ExecuteAsync(
-        "INSERT INTO Elevators (id, ConnectionString) VALUES (@deviceId, @connectionString)",
-        new {deviceId, ConnectionString = ""});
-}
-else
-{
-    Console.WriteLine("Device already exists. Getting connection string");
-}
-
-
-var deviceConnectionString = await connection.QueryFirstOrDefaultAsync<string>("SELECT ConnectionString FROM Elevators WHERE Id = @DeviceId", new { DeviceId = deviceId });
-if (string.IsNullOrEmpty(deviceConnectionString))
-{
-    Console.WriteLine("Initializing Connection string ...");
-
-    using var http = new HttpClient();
-    var result = await http.PostAsJsonAsync($"{connectUrl}?DeviceId={deviceId}", new { DeviceId = deviceId });
-
-    deviceConnectionString = await result.Content.ReadAsStringAsync();
-    await connection.ExecuteAsync("UPDATE Elevators SET ConnectionString = @ConnectionString WHERE Id = @DeviceId",
-         new { DeviceId = deviceId, ConnectionString = deviceConnectionString });
-}
-
-DeviceClient _deviceClient = DeviceClient.CreateFromConnectionString(IoTHubConnectionString, deviceId.ToString());
-
-Console.WriteLine("Updating twin properties ...");
-var twinCollection = new TwinCollection();
-twinCollection["deviceName"] = deviceName.ToLower();
-twinCollection["status"] = status.ToString().ToLower();
-twinCollection["doorStatus"] = doorStatus;
-twinCollection["currentLevel"] = currentLevel;
-twinCollection["targetLevel"] = targetLevel;
-await _deviceClient.UpdateReportedPropertiesAsync(twinCollection);
-
-var twin = await _deviceClient.GetTwinAsync();
-
-
-await _deviceClient.SetMethodHandlerAsync("ChangeLevel", ChangeLevel, null);
-
-connected = true;
-Console.WriteLine("Device Connected");
-
-while (true)
-{
-    twinCollection["update"] = "jag existerar";
-    await _deviceClient.UpdateReportedPropertiesAsync(twinCollection);
-
-    Console.WriteLine("Message sent");
-    await Task.Delay(interval);
-}
-
-
-Task<MethodResponse> ChangeLevel(MethodRequest methodRequest, object userContext)
-{
-    //var newLevel = userContext.
-    //doorStatus = false;
-    //targetLevel = newLevel;
-    //if (newLevel != currentLevel)
-    //{
-    //    twinCollection["doorStatus"] = doorStatus;
-    //    twinCollection["targetLevel"] = targetLevel;
-    //    _deviceClient.UpdateReportedPropertiesAsync(twinCollection);
-
-    //    while (currentLevel != targetLevel)
-    //    {
-    //        if (currentLevel > targetLevel)
-    //            currentLevel -= 1;
-    //        if (currentLevel < targetLevel)
-    //            currentLevel += 1;
-
-    //        Task.Delay(TimeSpan.FromSeconds(4));
-    //    }
-    //}
-
-    return Task.FromResult(new MethodResponse(new byte[0], 200));
 }
